62aa86253d243624c0cff36e786911fa6764c8e5
Mhmdmnsouri <bitshawcoder@gmail.com>
2022-09-03 14:41:58 +0430

    Update the project

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,14 +1,36 @@
 const endDate = document.querySelector("input[name='endDate']");
 const clock = document.querySelector(".clock");
+let timeInterval;
+let timeStop = true;
 
 endDate.addEventListener("change", function (e) {
     e.preventDefault();
-    let temp = new Date(endDate.value);
-    starClock(temp);
+    clearInterval(timeInterval);
+    const temp = new Date(endDate.value);
+    startClock(temp);
+    timeStop = true;
 });
 
-function starClock(d) {
-    console.log(timeLeft(d));
+function startClock(d) {
+    function updateCounter() {
+        let timeLine = (timeLeft(d));
+        if (timeLine.total <= 0) {
+            timeStop = false;
+        }
+
+        for (let i in timeLine) {
+            let elem = clock.querySelector("." + i);
+            if (elem) {
+                elem.innerHTML = timeLine[i];
+            }
+        }
+    }
+    updateCounter();
+    if (timeStop) {
+        timeInterval = setInterval(updateCounter, 1000);
+    } else {
+        clearInterval(timeInterval);
+    }
 }
 
 function timeLeft(d) {
@@ -21,7 +43,7 @@ function timeLeft(d) {
     console.log("Seconds : " + seconds);
     let minutes = Math.floor(((total / 1000) / 60) % 60);
     console.log("Minutes : " + minutes);
-    let hours = Math.floor(((total / 1000) * 60) % 24);
+    let hours = Math.floor(((total) / (1000 * 60 * 60)) % 24);
     console.log("Hours : " + hours);
     let days = Math.floor(total / (1000 * 60 * 60 * 24))
     console.log("Days : " + days);
